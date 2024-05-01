6e3cbd4c862beb001fabf99c18564985cd85c680
motos@ubits.co <millerandresoc@gmail.com>
2024-04-30 23:12:20 -0500

    remove secrets

--- backend/app/routers/predictions.py
+++ backend/app/routers/predictions.py
@@ -4,16 +4,16 @@ from app.ml.model import model
 from app.llm.llm_client import LLMClient
 
 router = APIRouter()
-llm_client = LLMClient(api_key="sk-proj-NepDxIXdGWraoXhIhQ7rT3BlbkFJA0JAa0M3XG55uoRSx1Fv")
+llm_client = LLMClient(api_key="")
 
 @router.post("/predict")
 async def predict(data: UserData):
-    print("Holaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+  
     try:
-        print("------------------>")
+        
         prediction = model(data.dict())
         recommendations = llm_client.get_recommendations(data.dict(), prediction)
         return {"prediction": prediction, "recommendations": recommendations}
     except Exception as e:
-        print("--____--------------->")
+        
         raise HTTPException(status_code=400, detail=str(e))
